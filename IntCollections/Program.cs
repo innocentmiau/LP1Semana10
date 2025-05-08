fa78d211d40c8f1b969a310b0d68543cdf63b1b2
André <imandreleandro@gmail.com>
2025-05-08 15:25:30 +0100

    abc

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -9,15 +9,20 @@ namespace IntCollections
         private static void Main(string[] args)
         {
             // List<T>, Stack<T>, Queue<T> e HashSet<T>
-            List<int> list = new List<int>() { 5, 10, 20, 50, -10 };
+            List<int> list = new List<int>() {1, 10, -30, 10, -5};
             Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(10);
+            stack.Push(-30);
+            stack.Push(10);
+            stack.Push(-5);
             Queue<int> queue = new Queue<int>();
-            queue.Enqueue(5);
+            queue.Enqueue(1);
             queue.Enqueue(10);
-            HashSet<int> hashSet = new HashSet<int>();
-            hashSet.Add(100);
-            hashSet.Add(5);
-            hashSet.Add(20);
+            queue.Enqueue(-30);
+            queue.Enqueue(10);
+            queue.Enqueue(-5);
+            HashSet<int> hashSet = new HashSet<int>() {1, 10, -30, 10, -5}; // duplicate value, 2nd 10 não irá aparecer.
 
             PrintCollection(list);
             PrintCollection(stack);
@@ -31,14 +36,10 @@ namespace IntCollections
             int backtickIndex = fullName.IndexOf('`');
             if (backtickIndex > 0) fullName = fullName.Substring(0, backtickIndex);
             StringBuilder stringBuilder = new StringBuilder($"{fullName}: ");
-            int count = 0;
             foreach (var item in collection)
             {
                 stringBuilder.Append(item + ", ");
-                count++;
             }
-
-            if (count == 0) stringBuilder.Append("None  ");
             Console.WriteLine(stringBuilder.ToString().Substring(0, stringBuilder.Length - 2));
         }
     }
